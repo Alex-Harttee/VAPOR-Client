45a96e17261fae3f004ca13beffcd49d35bb6fa3
Alex-Harttee <51974982+Alex-Harttee@users.noreply.github.com>
2019-06-19 19:31:46 -0400

    Add files via upload

---
+++ VAPOR Client/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Management;
+using System.Collections;
+using System.Net;
+using System.Net.NetworkInformation;
+using CryptoSysPKI;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace PCNetworkGrabber
+{
+    class GetInfo
+    {
+        static void getHardDriveInfo()
+        {
+            ArrayList hdCollection = new ArrayList();
+            ManagementObjectSearcher searcher = new
+            ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+
+            foreach (ManagementObject wmi_HD in searcher.Get())
+            {
+                HardDrive hd = new HardDrive();
+                hd.Model = wmi_HD["Model"].ToString();
+                hd.Type = wmi_HD["InterfaceType"].ToString();
+                hdCollection.Add(hd);
+            }
+
+            searcher = new
+            ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMedia");
+
+            int i = 0;
+            foreach (ManagementObject wmi_HD in searcher.Get())
+            {
+                // get the hard drive from collection
+                // using index
+                HardDrive hd = (HardDrive)hdCollection[i];
+
+                // get the hardware serial no.
+                if (wmi_HD["SerialNumber"] == null)
+                    hd.SerialNo = "None";
+                else
+                    hd.SerialNo = wmi_HD["SerialNumber"].ToString();
+
+                ++i;
+            }
+            static void Main(string[] args)
+            {
+                // Get host name
+                string strHostName = Dns.GetHostName();
+                string WinComputerName = System.Environment.MachineName.ToString();
+
+
+                foreach (NetworkInterface netInterface in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    string Date = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
+                    string PCName = WinComputerName.ToLower();
+
+                    if (netInterface.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || netInterface.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                    {
+
+                        foreach (UnicastIPAddressInformation ip in netInterface.GetIPProperties().UnicastAddresses)
+                        {
+                            if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                            {
+
+                                Console.WriteLine(Date + "," + ip.Address.ToString() + "," + netInterface.GetPhysicalAddress().ToString() + "," + WinComputerName + "," + strHostName);
+                            }
+                        }
+                    }
+
+                }
+                foreach (HardDrive hd in hdCollection)
+                {
+                    Console.WriteLine("Model\t\t: " + hd.Model);
+                    Console.WriteLine("Type\t\t: " + hd.Type);
+                    Console.WriteLine("Serial No.\t: " + hd.SerialNo);
+                    Console.WriteLine();
+                }
+
+
+            }
+        }
+    }
+}
---
+++ VAPOR Client/HardDrive.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace VAPOR
+{
+    /// <summary>
+    /// This "HardDrive" class will obtain the Serial ID of the the user's HardDrive using a GetHardDriveInfo() method.
+    /// </summary>
+    public class HardDrive
+    {
+        private string serialNo = null;
+        public string SerialNo
+        {
+            get { return serialNo; }
+            set { serialNo = value; }
+        }
+        public static String GetHardDriveInfo()
+        {
+            ArrayList hdCollection = new ArrayList();
+            ManagementObjectSearcher searcher = new
+            ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+
+            foreach (ManagementObject wmi_HD in searcher.Get())
+            {
+                HardDrive hd = new HardDrive();
+                hdCollection.Add(hd);
+            }
+
+            searcher = new
+            ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMedia");
+
+            int i = 0;
+            foreach (ManagementObject wmi_HD in searcher.Get())
+            {
+                // get the hard drive from collection
+                // using index
+                HardDrive hd = (HardDrive)hdCollection[i];
+
+                // get the hardware serial no.
+                if (wmi_HD["SerialNumber"] == null)
+                    hd.SerialNo = "None";
+                else
+                    hd.SerialNo = wmi_HD["SerialNumber"].ToString();
+
+                ++i;
+            }
+            foreach (HardDrive hd in hdCollection)
+            {
+                return hd.SerialNo.ToString();
+
+            }
+            return "null";
+        }
+    }
+}
+
